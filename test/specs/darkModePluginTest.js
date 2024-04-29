16668bc7e029af304f6a2f6b48ee10b1cbe3848f
TanvirHossain-eco <tanvir.echologyx@gmail.com>
2024-04-29 20:12:19 +0600

    WPPOOL Testing - 29/04/2024

--- test/specs/darkModePluginTest.js
+++ test/specs/darkModePluginTest.js
@@ -14,11 +14,12 @@ describe('Dark Mode Plugin Testing', () => {
         await $('#wp-submit').click()
         await browser.pause(3000)
     });
+    // Need to compress the code - From Here
     it('Check Whether or not WP Dark Mode Plugin is Installed & Activated', async() => {
         // await browser.maximizeWindow()
         // await browser.url('https://dev-wppoolstesting.pantheonsite.io/wp-admin/')
-        const installedPluginsMenu = await $('#menu-plugins')
-        await installedPluginsMenu.click()
+        
+        await $('#menu-plugins').click()
         await browser.pause(3000)
         // const darkmodeExists = await browser.isExisting()
         await $("input[type='search']").setValue('wp dark mode')
@@ -68,9 +69,11 @@ describe('Dark Mode Plugin Testing', () => {
             } else if (buttonText2.includes('Activate')) {
                 await button2.click()
                 console.log('Plugin was Installed Before, Now It is Activated')
+                await browser.pause(3000)
             } else {
                 console.log ('Plugin was Installed & Activated')
                 await browser.pause(3000)
+                
             }
             
         }else{
@@ -79,8 +82,10 @@ describe('Dark Mode Plugin Testing', () => {
         }        
            
     });
+    // Need to compress the code - to here
     it('Enable Backend Darkmode from Settings', async() => {
-        await $('#toplevel_page_wp-dark-mode a').click()
+        
+        await $('#toplevel_page_wp-dark-mode div.wp-menu-name').click()
         await browser.pause(3000)
         const darkMode = await $('#wp-admin-bar-wp-dark-mode-admin-bar-switch span:nth-of-type(2)')
         await darkMode.click()
@@ -101,8 +106,21 @@ describe('Dark Mode Plugin Testing', () => {
             console.log ('Dark Mode Color is not Verified & Current Color Code: ', actualDarkModeColor.parsed.hex)
         }
     });
-    it('Navigate to the WP Dark Mode', async() => {
-        
+    it('Navigate to the WP Dark Mode & Change Settings', async() => {
+        await $('#toplevel_page_wp-dark-mode div.wp-menu-name').click()
+        await browser.pause(3000)
+        const customizationElem = await $('div.wp-dark-mode-admin-sidebar-nav > div:nth-of-type(2) h4')
+        await customizationElem.click()
+        await browser.pause(3000)
+        const swSettingsElem = await $('div.wp-dark-mode-admin-sidebar-nav > div:nth-of-type(2) a:nth-of-type(1)')
+        await swSettingsElem.click()
+        await browser.pause(3000)
+        const selSwitchElem = await $('div.bg-transparent > div.gap-6 > div.rounded > div.flex-wrap > div:nth-of-type(3)')
+        await selSwitchElem.click()
+        await browser.pause(3000)
+        const saveBtn = await $('button.bg-blue-500')
+        await saveBtn.click()
+        await browser.pause(3000)
     });
 
 
