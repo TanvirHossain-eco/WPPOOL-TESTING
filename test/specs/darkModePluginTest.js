b496f8856c285a7818ea3aaaf7ed4f9fe785a9fd
TanvirHossain-eco <tanvir.echologyx@gmail.com>
2024-05-02 04:51:40 +0600

    WPPOOL Testing - 02/05/2024

--- test/specs/darkModePluginTest.js
+++ test/specs/darkModePluginTest.js
@@ -15,73 +15,73 @@ describe('Dark Mode Plugin Testing', () => {
         await browser.pause(3000)
     });
     // Need to compress the code - From Here
-    it('Check Whether or not WP Dark Mode Plugin is Installed & Activated', async() => {
-        // await browser.maximizeWindow()
-        // await browser.url('https://dev-wppoolstesting.pantheonsite.io/wp-admin/')
+    // it('Check Whether or not WP Dark Mode Plugin is Installed & Activated', async() => {
+    //     // await browser.maximizeWindow()
+    //     // await browser.url('https://dev-wppoolstesting.pantheonsite.io/wp-admin/')
         
-        await $('#menu-plugins').click()
-        await browser.pause(3000)
-        // const darkmodeExists = await browser.isExisting()
-        await $("input[type='search']").setValue('wp dark mode')
-        await browser.pause(3000)
-        const searchResults = await $('#the-list td').getText()
-        // const search2Results = await $('#activate-wp-dark-mode').getText()
-        if (searchResults.includes('No plugins found for: wp dark mode.')){
-            console.log('Plugin is not Installed Yet')
-            await browser.pause(3000)
-            await $('.page-title-action').click()
+    //     await $('#menu-plugins').click()
+    //     await browser.pause(3000)
+    //     // const darkmodeExists = await browser.isExisting()
+    //     await $("input[type='search']").setValue('wp dark mode')
+    //     await browser.pause(3000)
+    //     const searchResults = await $('#the-list td').getText()
+    //     // const search2Results = await $('#activate-wp-dark-mode').getText()
+    //     if (searchResults.includes('No plugins found for: wp dark mode.')){
+    //         console.log('Plugin is not Installed Yet')
+    //         await browser.pause(3000)
+    //         await $('.page-title-action').click()
             
-        } else {
-            const search2Results = await $('#the-list td span a').getText()
-            if (search2Results.includes('Activate')){
-                const button1 = await $('#activate-wp-dark-mode')
-                await button1.click()
-                await browser.pause(3000)
-                console.log ('Plugin is Installed & Activated')
-                await browser.pause(3000)
-                await $('#menu-plugins').click()
-                await $('.page-title-action').click()
-            }else {
-                console.log ('Plugin was Installed & Activated')
-                await browser.pause(3000)
-                await $('#menu-plugins').click()
-                await $('.page-title-action').click()
-            }
+    //     } else {
+    //         const search2Results = await $('#the-list td span a').getText()
+    //         if (search2Results.includes('Activate')){
+    //             const button1 = await $('#activate-wp-dark-mode')
+    //             await button1.click()
+    //             await browser.pause(3000)
+    //             console.log ('Plugin is Installed & Activated')
+    //             await browser.pause(3000)
+    //             await $('#menu-plugins').click()
+    //             await $('.page-title-action').click()
+    //         }else {
+    //             console.log ('Plugin was Installed & Activated')
+    //             await browser.pause(3000)
+    //             await $('#menu-plugins').click()
+    //             await $('.page-title-action').click()
+    //         }
             
-        } 
+    //     } 
                    
-    });
-    it('Find & Verify WP Dark Mode Plugin is Installed & Activated', async() => {
-        await $("input[type='search']").setValue('wp dark mode')
-        await browser.pause(3000)
-        const searchResults2 = await $('div.plugin-card-wp-dark-mode div.desc a').getText()
-        if (searchResults2.includes('WPPOOL')){
-            const button2= await $('div.plugin-card-wp-dark-mode div.action-links li .button')
-            const buttonText2 = await button2.getText()
-            if (buttonText2.includes('Install Now')){
-                await button2.click()
-                console.log('Plugin is Installed')
-                await browser.pause(3000)
-                await button2.click()
-                console.log('Plugin is Installed & Activated')
-                await browser.pause(3000)
+    // });
+    // it('Find & Verify WP Dark Mode Plugin is Installed & Activated', async() => {
+    //     await $("input[type='search']").setValue('wp dark mode')
+    //     await browser.pause(3000)
+    //     const searchResults2 = await $('div.plugin-card-wp-dark-mode div.desc a').getText()
+    //     if (searchResults2.includes('WPPOOL')){
+    //         const button2= await $('div.plugin-card-wp-dark-mode div.action-links li .button')
+    //         const buttonText2 = await button2.getText()
+    //         if (buttonText2.includes('Install Now')){
+    //             await button2.click()
+    //             console.log('Plugin is Installed')
+    //             await browser.pause(3000)
+    //             await button2.click()
+    //             console.log('Plugin is Installed & Activated')
+    //             await browser.pause(3000)
                 
-            } else if (buttonText2.includes('Activate')) {
-                await button2.click()
-                console.log('Plugin was Installed Before, Now It is Activated')
-                await browser.pause(3000)
-            } else {
-                console.log ('Plugin was Installed & Activated')
-                await browser.pause(3000)
+    //         } else if (buttonText2.includes('Activate')) {
+    //             await button2.click()
+    //             console.log('Plugin was Installed Before, Now It is Activated')
+    //             await browser.pause(3000)
+    //         } else {
+    //             console.log ('Plugin was Installed & Activated')
+    //             await browser.pause(3000)
                 
-            }
+    //         }
             
-        }else{
-            console.log ('Plugin is not Found')
-            await browser.pause(3000)
-        }        
+    //     }else{
+    //         console.log ('Plugin is not Found')
+    //         await browser.pause(3000)
+    //     }        
            
-    });
+    // });
     // Need to compress the code - to here
     it('Enable Backend Darkmode from Settings', async() => {
         
@@ -106,7 +106,7 @@ describe('Dark Mode Plugin Testing', () => {
             console.log ('Dark Mode Color is not Verified & Current Color Code: ', actualDarkModeColor.parsed.hex)
         }
     });
-    it('Navigate to the WP Dark Mode & Change Settings', async() => {
+    it('Navigate to the WP Dark Mode & Change Floation Switch Style Settings', async() => {
         await $('#toplevel_page_wp-dark-mode div.wp-menu-name').click()
         await browser.pause(3000)
         const customizationElem = await $('div.wp-dark-mode-admin-sidebar-nav > div:nth-of-type(2) h4')
@@ -116,32 +116,99 @@ describe('Dark Mode Plugin Testing', () => {
         await swSettingsElem.click()
         await browser.pause(3000)
         const switchElem1 = await $('div.bg-transparent > div.gap-6 > div.rounded > div.flex-wrap > div:nth-of-type(1)')
-        const selSwitchElem1 = await $('div.bg-transparent > div.gap-6 > div.rounded > div.flex-wrap > div:nth-of-type(1)').getAttribute('class')
+        const selSwitchElem1 = await switchElem1.getAttribute('class')
         const switchElem2 = await $('div.bg-transparent > div.gap-6 > div.rounded > div.flex-wrap > div:nth-of-type(2)')
-        const selSwitchElem2 = await $('div.bg-transparent > div.gap-6 > div.rounded > div.flex-wrap > div:nth-of-type(2)').getAttribute('class')
+        const selSwitchElem2 = await switchElem2.getAttribute('class')
         const switchElem3 = await $('div.bg-transparent > div.gap-6 > div.rounded > div.flex-wrap > div:nth-of-type(3)')
-        const selSwitchElem3 = await $('div.bg-transparent > div.gap-6 > div.rounded > div.flex-wrap > div:nth-of-type(3)').getAttribute('class')
-        const saveBtn = await $('button.bg-blue-500');
+        const selSwitchElem3 = await switchElem3.getAttribute('class')
+        const saveBtn1 = await $('button.bg-blue-500');
         if (selSwitchElem1.includes('_selected')){
             await switchElem2.click()
             console.log('2nd Tile is Selected')
-            await saveBtn.click()
+            await saveBtn1.click()
             await browser.pause(3000)
         } else if (selSwitchElem2.includes('_selected')){
             await switchElem3.click()
             console.log('3rd Tile is Selected')
-            await saveBtn.click()
+            await saveBtn1.click()
             await browser.pause(3000)
         } else {
             await switchElem1.click()
             console.log('1st Tile is Selected')
-            await saveBtn.click()
+            await saveBtn1.click()
             await browser.pause(3000)
         }
-                
+        // await browser.debug()
+
     });
     it('Switch Settings - Select Custom Switch size & Scale it to 220', async() => {
+        const floatingElem1 = await $('div.gap-8 div.flex > div:nth-of-type(1) > div.gap-1')
+        await floatingElem1.scrollIntoView()
+        await browser.pause(3000)
+        const customElem = await $('div:nth-of-type(6) > span')
+        const customFlSwElem = await customElem.getAttribute('class')
+        // await customFlSwElem.click()
+        const saveBtn2 = await $('button.bg-blue-500');
+        const scaleValue1 = await $('div.min-w-56 > input')
+        // await saveBtn2.click()
+        await browser.pause(3000)
+        if (!customFlSwElem.includes('shadow-md')){
+            await customElem.click()
+            await scaleValue1.dragAndDrop({x: 46, y: 0})
+            await saveBtn2.click()
+            console.log('Custom Switch is Selected & Scale value is set to 220')
+            await browser.pause(3000)
+        } else if (customFlSwElem.includes('shadow-md')){
+            await scaleValue1.dragAndDrop({x: 46, y: 0})
+            await saveBtn2.click()
+            console.log('Custom Switch Was Selected & Scale value is set to 220')
+            await browser.pause(3000)
+        }
+        else{
+            console.log('Custom Switch was Selected & Scale value was set to 220')
+            await browser.pause(3000)
+        }        
         
+    });
+    it('Switch Settings - Change the Floating Switch Position (Left Bottom)', async() => {
+        const switchPosition = await $('div.gap-8 > div > div.flex > div:nth-of-type(2) > div.flex-col')
+        await switchPosition.scrollIntoView()
+        await browser.pause(3000)
+        const leftPosition = await $('div.gap-8 > div > div.flex > div:nth-of-type(2) div.bg-gray-100 > span')
+        const selLeftPosition = await leftPosition.getAttribute('class')
+        const saveBtn3 = await $('button.bg-blue-500');
+        if (!selLeftPosition.includes('shadow-md')){
+            await leftPosition.click()
+            await saveBtn3.click()
+            console.log('Floating Switch Position is selected Left Bottom')
+            await browser.pause(3000)
+        } else {
+            console.log('Floating Switch Position was selected Left Bottom')
+            await browser.pause(3000)
+        }
+
+    });
+    it('Disable Keyboard Shortcut from the Accessibility Settings', async() => {
+        const advancedElem = await $('div.wp-dark-mode-admin-sidebar div:nth-of-type(3) > div > div.items-center')
+        await advancedElem.click()
+        await browser.pause(3000)
+        const accessibilityElem = await $('div.wp-dark-mode-admin-sidebar-nav > div:nth-of-type(3) a:nth-of-type(4)')
+        await accessibilityElem.click()
+        const accessSettings = await $('#wpbody div:nth-of-type(5)')
+        await accessSettings.scrollIntoView()
+        await browser.pause(3000)
+        const keyboardShortcut = await $('div.main-content-body > div > div.gap-5 > div:nth-of-type(1) div.w-auto > div')
+        const switchKeyboardShortcut = await keyboardShortcut.getAttribute('class')
+        if (!switchKeyboardShortcut.includes('bg-slate-200')){
+            await keyboardShortcut.click()
+            const saveBtn4 = await $('div.save-buttons > button.bg-blue-500');
+            await saveBtn4.click()
+            console.log('Keyboard Shortcut is Disabled')
+            await browser.pause(3000)
+        } else {
+            console.log('Keyboard Shortcut was Disabled')
+            await browser.pause(3000)
+        }          
     });
 });
 
--- test/specs/loginTest.js
+++ test/specs/loginTest.js
@@ -25,6 +25,20 @@ describe('Wp Pool Login', () => {
         const color = await elem.getCSSProperty('color')
         console.log('color is: ', color.parsed.hex)
     });
+    it('Switch Settings - Select Custom Switch size & Scale it to 220', async() => {
+        const floatingElem1 = await $('div.gap-8 div.flex > div:nth-of-type(1) > div.gap-1')
+        await floatingElem1.scrollIntoView()
+        await browser.pause(3000)
+        const customFlSwElem = await $('div:nth-of-type(6) > span')
+        await customFlSwElem.click()
+        const saveBtn2 = await $('button.bg-blue-500');
+        const scaleValue1 = await $('div.min-w-56 > input')
+        await scaleValue1.dragAndDrop({x: 46, y: 0})
+        console.log('Scale Value is: ', scaleValue1)
+        await saveBtn2.click()
+        console.log('Custom Switch is Selected & Scale value is set to 220')
+        await browser.pause(3000)
+    });
 })
 // console.log('selSwitchElem1 => ', selSwitchElem1);
 // console.log("el 1 => ", selSwitchElem1);
